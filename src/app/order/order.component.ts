87d4bea4dc2709dd2952270ee71a6020c526928e
Leonardo Campos Alves <leonardo.alves@sinapsisenergia.com>
2019-04-18 20:05:36 -0300

    Adicionado Reactive Forms e Modulos

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -5,6 +5,7 @@ import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'mt-order',
@@ -12,25 +13,61 @@ import { Router } from '@angular/router';
 })
 export class OrderComponent implements OnInit {
 
+
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  numberPattern = /^[0-9]*$/
+  orderForm: FormGroup
+
   delivery: number = 8
 
   paymentOptions: RadioOption[] = [
-            {label: 'Dinheiro', value: 'Mon'},
-            {label: 'Cartão de Débito', value: 'DEB'},
-            {label: 'Cartão Regeição', value: 'REF'}
-          
-          ]
+    { label: 'Dinheiro', value: 'Mon' },
+    { label: 'Cartão de Débito', value: 'DEB' },
+    { label: 'Cartão Regeição', value: 'REF' }
+
+  ]
 
 
-          
-  constructor(private orderService: OrderService, private router: Router) { }
+
+  constructor(private orderService: OrderService,
+    private router: Router,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    this.orderForm = this.formBuilder.group(
+      {
+        name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+        email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+        emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+        address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+        number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
+        optionalAddress: this.formBuilder.control(''),
+        paymentOption: this.formBuilder.control('', [Validators.required])
+      },
+      {validator: OrderComponent.equalsTo})
+
   }
 
-  checkOrder(order: Order){
-    
-    order.orderItems = this.cartItems().map((item:CartItem)=> new OrderItem(item.quantity, item.menuItem.id))
+  static equalsTo(group: AbstractControl): {[key: string]: boolean} {
+
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+
+      if (!email || !emailConfirmation){
+        return undefined
+      }
+
+
+      if(email.value !== emailConfirmation.value){
+        return {emailsNotMatch:true}
+      }
+      return undefined
+  }
+
+  checkOrder(order: Order) {
+
+    order.orderItems = this.cartItems().map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
     this.orderService.checkOrder(order).subscribe((orderId: string) => {
 
       this.router.navigate(['/order-summary'])
@@ -45,20 +82,20 @@ export class OrderComponent implements OnInit {
     return this.orderService.itemsValue()
   }
 
-  cartItems(): CartItem[]{
+  cartItems(): CartItem[] {
     return this.orderService.cartItems()
   }
 
-  increaseQty(item: CartItem){
+  increaseQty(item: CartItem) {
     this.orderService.increaseQty(item)
   }
-  
-  
-  decreaseQty(item: CartItem){
+
+
+  decreaseQty(item: CartItem) {
     this.orderService.decreaseQty(item)
   }
 
-  remove(item: CartItem){
+  remove(item: CartItem) {
     this.orderService.remove(item)
   }
 
--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import { NgModel, FormControlName } from '@angular/forms';
 
 @Component({
   selector: 'mt-input-container',
@@ -14,6 +14,8 @@ export class InputComponent implements OnInit, AfterContentInit {
   @Input() errorMessage: string
 
   @ContentChild(NgModel) model: NgModel
+  @ContentChild(FormControlName) control: FormControlName
+
 
   constructor() { }
 
@@ -21,9 +23,9 @@ export class InputComponent implements OnInit, AfterContentInit {
   }
 
   ngAfterContentInit(): void {
-    this.input = this.model
+    this.input = this.model || this.control
     if (this.input === undefined) {
-      throw new Error('Esse componente precisa ser usado com uma diretiva NgModel')
+      throw new Error('Esse componente precisa ser usado com uma diretiva NgModel ou formControlName')
     }
   }
 
