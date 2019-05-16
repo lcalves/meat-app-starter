53cdaa3705779a3b81e747dc831cf9ea617909dc
Leonardo Campos Alves <leonardo.alves@sinapsisenergia.com>
2019-05-15 21:33:51 -0300

    filtrando por restaurante

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,7 +1,17 @@
+
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Restaurant } from './restaurant/restaurant.model'
 import { RestaurantsService } from './restaurants.service'
 import { trigger, state, style, transition, animate } from '@angular/animations';
+
+import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/do' 
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/operator/from'
+import { Observable } from 'rxjs/Observable';
 
 
 @Component({
@@ -23,13 +33,33 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 
 export class RestaurantsComponent implements OnInit {
 
+
+
   searchBarState = 'hidden'
 
   restaurants: Restaurant[]
 
-  constructor(private restaurantsService: RestaurantsService) { }
+  searchForm: FormGroup
+  searchControl: FormControl
+
+  constructor(private restaurantsService: RestaurantsService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+      this.searchControl = this.formBuilder.control('')
+
+      this.searchForm = this.formBuilder.group({
+        searchControl: this.searchControl
+      })
+
+      this.searchControl.valueChanges
+                        .debounceTime(500)
+                        .distinctUntilChanged()
+                        .switchMap(searchTerm => 
+                          this.restaurantsService
+                            .restaurants(searchTerm)
+                            .catch(error => Observable.from([])))
+                        .subscribe(restaurants => this.restaurants = restaurants)
 
       this.restaurantsService.restaurants().subscribe(rest => this.restaurants = rest)
 
